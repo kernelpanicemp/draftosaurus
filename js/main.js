5b26605d2e1afe06eadd1148ce78b7725301759b
Kernel Panic <kernelpanicemp@gmail.com>
2025-07-11 00:16:30 -0300

    Fix some responsive issues

--- js/main.js
+++ js/main.js
@@ -2,26 +2,49 @@
 // Todas las pantallas están ocultas por defecto así que las voy mostrando con esto
 // Va una por una en base al id, después voy a cambiar todo esto
 // Para el prototipo inicial esto funciona bien
-function launchConfetti() {
-  confetti({
-    particleCount: 100,
-    spread: 180,
-    origin: { y: 0.6 },
-  })
-}
-
 function showScreen(screenId) {
-  document.querySelectorAll('#content > div').forEach((div) => {
-    div.classList.add('hidden')
-    div.classList.remove('flex')
+  document.querySelectorAll('[id^="screen-"]').forEach((screen) => {
+    screen.classList.add('hidden')
+    screen.classList.remove('flex')
   })
 
   const screenToShow = document.getElementById(screenId)
   if (screenToShow) {
     screenToShow.classList.remove('hidden')
     screenToShow.classList.add('flex')
-    if (screenId === 'screen-results') {
-      launchConfetti()
+  }
+
+  if (screenId === 'screen-results') {
+    const duration = 5 * 1000
+    const animationEnd = Date.now() + duration
+    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 100 }
+
+    function randomInRange(min, max) {
+      return Math.random() * (max - min) + min
     }
+
+    const interval = setInterval(function() {
+      const timeLeft = animationEnd - Date.now()
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval)
+      }
+
+      const particleCount = 50 * (timeLeft / duration)
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+      })
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+      })
+    }, 250)
   }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  showScreen('screen-home')
+})
